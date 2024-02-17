9ba9ca1ad98401b9bad8f2c20b04da0b3b53ebdb
Sewon Kim <sewonkim0black@gmail.com>
2024-02-17 17:44:40 -0500

    Img uploading system (client -> server) implementation complete, way to access img is unknown

--- Smartcycle/urls.py
+++ Smartcycle/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("welcome/", views.handler),
+    path("upload/", views.upload_image)
 ]
